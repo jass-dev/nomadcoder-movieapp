dc4b739477192acbcc9822ea97c65430b2af924b
unknown <mstoryfjqm@naver.com>
2020-08-26 23:02:41 +0900

    second day of react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Potato from "./Potato";
+import propTypes from "prop-types";
+
+function Food2(props) {
+  return <div>
+      <h1> FOOD2 : I like {props.name} again</h1>
+      <h1>  {props.desc} </h1>
+    </div>
+}
+
+function Food({name, desc}) {
+  return <div>
+      <h1> FOOD1 : I like {name} again</h1>
+      <h1>  {desc} </h1>
+    </div>
+}
+
+function renderFood (dish){
+  return <Food key={dish.id} name={dish.name} desc={dish.desc}></Food>
+}
+
+Food.propTypes={
+  name : propTypes.string.isRequired,
+  desc : propTypes.string.isRequired,
+  rating : propTypes.number.isRequired
+};
+
+const foodILike = [{id:"1", name : "kimchi", desc : "it is kimchi", rating:4}, {id:"2",name : "smagiopsal", desc : "it is pork",rating:3}, {id:"3",name : "pidzza", desc : "it is pizza",rating:2}]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello
+      <Potato/>
+      <Food key="0" name="kimch"/>
+      <Food2 key="0" name="samkyeopsal"/>
+      <Food2 key="5" name="pizza"/>
+      {foodILike.map(dish => <Food2 key={dish.id} name={dish.name + "!"} desc = {dish.desc}/>)}
+      {foodILike.map(dish => <Food key={dish.id} name={dish.name + "!"} desc = {dish.desc}/>)}
+
+      {foodILike.map(renderFood)}
     </div>
+    
   );
 }
 
---
+++ src/Potato.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Potato(){
+    return (
+        <h1>love potato</h1>
+    )
+}
+
+export default Potato;
